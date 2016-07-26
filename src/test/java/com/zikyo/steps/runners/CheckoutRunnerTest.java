0efe32e9671d187873f59ec10ce8ec545b20a9fb
dcosman <cosmandaniel@gmail.com>
2016-07-26 11:06:28 +0300

    updated checkoutrunner

--- src/test/java/com/zikyo/steps/runners/CheckoutRunnerTest.java
+++ src/test/java/com/zikyo/steps/runners/CheckoutRunnerTest.java
@@ -1,10 +1,10 @@
 package com.zikyo.steps.runners;
 
-
-
-
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 
 @CucumberOptions(
         monochrome = true,
